08870d2c3317b13c7a22af35fdb31be5a41b1b0e
z3on <z3on.ua@gmail.com>
2017-03-10 11:00:52 +0100

    Refactor problems 4 & 5 - make tail-recursive

--- src/main/scala/ListProblems.scala
+++ src/main/scala/ListProblems.scala
@@ -31,17 +31,23 @@ object ListProblems {
   /**
     * P04 (*) Find the number of elements of a list.
     */
-  def length[T](list: List[T]): Int = list match {
-    case Nil => 0
-    case _ :: tail => 1 + length(tail)
+  def length[T](list: List[T]): Int = {
+    def lengthRecursive(rest: List[T], curLength: Int): Int = rest match {
+      case Nil => curLength
+      case _ :: tail => lengthRecursive(tail, curLength + 1)
+    }
+    lengthRecursive(list, 0)
   }
 
   /**
     * P05 (*) Reverse a list.
     */
-  def reverse[T](list: List[T]): List[T] = list match {
-    case Nil => Nil
-    case head :: tail => reverse(tail) ++ List(head)
+  def reverse[T](list: List[T]): List[T] = {
+    def reverseRecursive(rest: List[T], reversed: List[T]): List[T] = rest match {
+      case Nil => reversed
+      case head :: tail => reverseRecursive(tail, head :: reversed)
+    }
+    reverseRecursive(list, Nil)
   }
 
   /**
